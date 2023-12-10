de2dc6eb3bdbd5f4ebb8ba299f4ea344fd01144c
Siddhant Patel <siddhantpatel445@gmail.com>
2023-12-10 23:30:30 +0530

    Java Phase 1 First Program Say Hello

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import phase1corefoundation.FirstProgram;
+
+public class Main {
+    public static void main(String[] args) {
+        FirstProgram.sayHello();
+    }
+}
---
+++ src/phase1corefoundation/FirstProgram.java
@@ -0,0 +1,20 @@
+package phase1corefoundation;
+
+public class FirstProgram { // this is a simple class
+    /* created a basic function
+    what ?
+    function a block of code to design a perform a specific task
+    why ?
+    Reuse of Code,
+    Modularity,
+    Abstraction,
+    Readability,
+    Encapsulation,
+    Parameterization,
+    Testing and Debugging
+    **/
+    public static void sayHello(){
+        System.out.println("Hello");
+    }
+    /*this function is called in main*/
+}
